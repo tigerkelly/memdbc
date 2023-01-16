4a3c4307a5f9f666c2022d1585af44a1a8d37fc6
Kelly Wiles <rkwiles@twc.com>
2023-01-16 09:21:02 -0600

    Added more docs and corrected some docs.

--- trietree.h
+++ trietree.h
@@ -18,9 +18,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * cqueue.c
- *
- * A circular fifo queue.
  *
  *  Created on: Jan 16, 2023
  *      Author: Kelly Wiles
